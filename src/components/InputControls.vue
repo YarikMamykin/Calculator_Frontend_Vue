aa3ef40dfe360d99a3ba83a20b3febd96df2955f
YarikM <ymamykin@gmail.com>
2022-01-15 12:58:12 +0200

    Implement InputControls component
    InputControls gathers all input buttons under on layout

---
+++ src/components/InputControls.vue
@@ -0,0 +1,38 @@
+<template>
+		<div class="controls"> 
+			<NumericPad/>
+      <OperationsPad/>
+      <SubmitButton/>
+    </div>
+</template>
+
+<script>
+import OperationsPad from './OperationsPad.vue';
+import NumericPad from './NumericPad.vue';
+import SubmitButton from './SubmitButton.vue';
+
+export default {
+  name: 'InputControls',
+  components: {
+    NumericPad,
+    OperationsPad,
+		SubmitButton
+  }
+}
+</script>
+
+<style>
+div.controls {
+	background-color: #000;
+	display: grid;
+	grid-template-columns: repeat(3, auto);
+	grid-row-gap: 10px;
+}
+
+div.pad {
+	background-color: black;
+	height: auto;
+}
+
+</style>
+
