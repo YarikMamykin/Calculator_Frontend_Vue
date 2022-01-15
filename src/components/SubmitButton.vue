42594e9d4cd3bb40149d299246d40673536750b4
YarikM <ymamykin@gmail.com>
2022-01-15 12:53:54 +0200

    Implement SubmitButton component
    SubmitButton represents '=' button that sends user input to BE for
    calculation

---
+++ src/components/SubmitButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <button id="bsubmit" @click="buttonPressed">=</button>
+</template>
+
+<script>
+export default {
+  name: 'SubmitButton',
+  methods:{
+    buttonPressed() {
+			this.$store.commit('inputHandler/handle', 'Enter');
+    }
+  }
+}
+</script>
+
+<style>
+button#bsubmit {
+  grid-column: 3;
+  grid-row: 1;
+  height: auto;
+  width: 3vw;
+  background-color: #222222;
+  color: #aaaaaa;
+  font-size: 5vh;
+  border: solid #202020 3px;
+}
+
+button:hover#bsubmit {
+  background-image: linear-gradient(to right, #00f000, #00acfc);
+  border-image-source: linear-gradient(to right, #00f000, #00acfc);
+  -webkit-background-clip: text;
+  border-image-slice: 1;
+  -webkit-text-fill-color: transparent;
+}
+</style>
+
