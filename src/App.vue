3f86d3e4ff5cd05980d615195f93321f4ca0d5a2
YarikM <ymamykin@gmail.com>
2022-02-07 00:35:51 +0200

    Place router-links under ViewNavigation component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="view_navigation">
-    <router-link to="/">
-      <input type="checkbox" id="view_selector_button" checked>
-      <label id="simple_view_selector">Simple</label>
-    </router-link>
-  </div>
+  <ViewNavigation/>
   <router-view/>
 </template>
+
+<script>
+import ViewNavigation from '@/components/ViewNavigation';
+
+export default {
+  name: 'App',
+  components: {
+    ViewNavigation
+  }
+}
+</script>
 
 <style>
 body {
   display: block;
 	background-color: black;
 }
-
-div#view_navigation {
-  display: inline-block;
-  margin-bottom: 20px;
-  margin-top: 10px;
-}
-
-input[type=checkbox] {
-  width: 100px;
-  height: 100px;
-}
-
 </style>
