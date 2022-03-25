13485e2d392ab38f4746453271f4c08cd9b45d10
YarikM <ymamykin@gmail.com>
2022-03-26 00:37:16 +0200

    Implement TimeOperand
    TimeOperand contains set of TimeUnits which represent certain timestamp

---
+++ src/components/time/TimeOperand.vue
@@ -0,0 +1,35 @@
+<template>
+	<div id="time_operand">
+		<TimeUnit hint="years" v-bind:allowedValues="[0,1,2,3,4]"/>
+		<TimeUnit hint="weeks" v-bind:allowedValues="[0,1,2,3,4]"/>
+		<TimeUnit hint="days" v-bind:allowedValues="[0,1,2,3,4]"/>
+		<TimeUnit hint="hours" v-bind:allowedValues="[0,1,2,3,4]"/>
+		<TimeUnit hint="minutes" v-bind:allowedValues="[0,1,2,3,4]"/>
+		<TimeUnit hint="seconds" v-bind:allowedValues="[0,1,2,3,4]"/>
+	</div>
+</template>
+
+<script>
+import TimeUnit from '@/components/time/TimeUnit.vue';
+
+export default {
+  name: 'TimeOperand',
+  components: {
+		TimeUnit
+  },
+}
+</script>
+
+<style scoped>
+#time_operand {
+	display: grid;
+	background-color: black;
+	grid-template-columns: repeat(6, auto);
+	grid-gap: 1vw;
+	width: 100%;
+	justify-content: center;
+	margin-bottom: 10vh;
+}
+</style>
+
+
